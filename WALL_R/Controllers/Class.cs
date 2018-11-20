8fadb2038c5df75a3f318b85cdd7af8add9e1ff2
Nico <fia63heber@gso-koeln.de>
2018-11-20 19:49:04 +0100

    Admincontroller done for now

---
+++ WALL_R/Controllers/Class.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WALL_R.Models;
+
+namespace WALL_R.Controllers
+{
+    [Route("api/[controller]")]
+    public class AdminController : Controller
+    {
+        private room_management_dbContext getContext()
+        {
+            return new room_management_dbContext();
+        }
+
+        [HttpPost("room")]
+        public IActionResult CreateRoom(string room_number)
+        {
+            room_management_dbContext context = getContext();
+            Rooms room = new Rooms();
+
+            room.RoomNumber = room_number;
+            context.Add(room);
+            context.SaveChanges();
+
+            return Ok();
+        }
+
+
+
+        [HttpPost("room/newOwner")]
+        public IActionResult SetOwnerForRoom(int room_id, int owner_id)
+        {
+            room_management_dbContext context = getContext();
+            if (context.Rooms.Where(f => f.Id == room_id).Count() == 0)
+            {
+                return NotFound();
+            }
+
+            context.Rooms.Where(f => f.Id == room_id).First().OwnerId = owner_id;
+            return Ok();
+            context.SaveChanges();
+        }
+    }
+}
