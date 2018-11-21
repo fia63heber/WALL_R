e1257aeec1539c4cb4df2866b9f8b68f3cd3e6e5
Nico <fia63heber@gso-koeln.de>
2018-11-21 18:21:24 +0100

    Owner Controller done for now

--- WALL_R/Controllers/OwnerController.cs
+++ WALL_R/Controllers/OwnerController.cs
@@ -35,14 +35,14 @@ namespace WALL_R.Controllers
             return Ok(defects);
         }
 
-        [HttpPost("defect/changePriority")]
+        [HttpPost("owner/defects")]
         public IActionResult GetDefectsForOwner(int defect_id, int priority_id)
         {
             room_management_dbContext context = getContext();
 
             context.Defects.Where(f => f.Id == defect_id).First().PriorityId = priority_id;
             context.SaveChanges();
-            
+
             return Ok();
         }
 
@@ -69,16 +69,116 @@ namespace WALL_R.Controllers
 
 
             int? file_id = context.Rooms.Where(f => f.Id == room_id).First().PictureFileId;
-            
+
             if (file_id == null)
             {
                 Files newFile = new Files();
                 newFile.FilePath = "";
+                //context.Add()
             }
 
             context.Files.Where(f => f.Id == file_id).First().FilePath = "";
 
             return Ok();
         }
+
+        [HttpPost("room/addDevice")]
+        public IActionResult addDeviceForRoom(int room_id, int device_id, int device_type_id, string name)
+        {
+            room_management_dbContext context = getContext();
+            bool error = false;
+            string error_message = "Fehlerhafte Angaben:\n";
+
+            if (context.Rooms.Where(f => f.Id == room_id).Count() == 0)
+            {
+                error = true;
+                error_message += "- Es wurde kein gültiger Raum angegeben\n";
+            }
+            if (context.DeviceTypes.Where(f => f.Id == device_type_id).Count() == 0)
+            {
+                error = true;
+                error_message += "- Es wurde kein gültiger Geräte-Typ angegeben\n";
+            }
+
+            if (error)
+            {
+                return NotFound(error_message);
+            }
+            Devices device = new Devices();
+            device.RoomId = room_id;
+            device.DeviceTypeId = device_type_id;
+            device.Name = name;
+
+            context.Devices.Add(device);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost("device/addComponent")]
+        public IActionResult addComponentForDevice(int device_id, int component_id, int component_type_id, string name, string serial_number)
+        {
+            room_management_dbContext context = getContext();
+            bool error = false;
+            string error_message = "Fehlerhafte Angaben:\n";
+
+            if (context.Devices.Where(f => f.Id == device_id).Count() == 0)
+            {
+                error = true;
+                error_message += "- Es wurde kein gültiges Gerät angegeben\n";
+            }
+            if (context.ComponentTypes.Where(f => f.Id == component_type_id).Count() == 0)
+            {
+                error = true;
+                error_message += "- Es wurde kein gültiger Komponent-Typ angegeben\n";
+            }
+
+            if (error)
+            {
+                return NotFound(error_message);
+            }
+            Components component = new Components();
+            component.DeviceId = device_id;
+            component.ComponentTypeId = component_type_id;
+            component.Name = name;
+
+            context.Components.Add(component);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet("defecttypes")]
+        public IActionResult GetAllDefectTypes()
+        {
+            room_management_dbContext context = getContext();
+
+            return Ok(context.DefectTypes);
+        }
+
+        [HttpDelete("defecttype")]
+        public IActionResult DeleteDefectType(int defect_type_id)
+        {
+            room_management_dbContext context = getContext();
+
+            DefectTypes defectTypeToDelete = context.DefectTypes.Where(f => f.Id == defect_type_id).First();
+            context.DefectTypes.Remove(defectTypeToDelete);
+            context.SaveChanges();
+
+            return Ok();
+        }
+
+
+        [HttpPost("defecttype")]
+        public IActionResult UpdateDefectType(string defect_type_name)
+        {
+            room_management_dbContext context = getContext();
+
+            DefectTypes defectType = new DefectTypes();
+            defectType.Name = defect_type_name;
+
+            context.DefectTypes.Add(defectType);
+            context.SaveChanges();
+
+            return Ok();
+        }  
     }
 }
